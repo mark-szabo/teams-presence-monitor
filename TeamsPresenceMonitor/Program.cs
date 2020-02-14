c0320e9297a9cb4f6e3e6eca2e5e4722262783d2
Mark Szabo <a-marks@microsoft.com>
2020-02-14 16:36:39 +0100

    Project init

---
+++ TeamsPresenceMonitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamsPresenceMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
